8bc8e0ad4ed0084cadad5d41c121996e681ef497
xp <74288201+xp0108@users.noreply.github.com>
2022-05-19 21:04:25 +0800

    rmv std::

--- Array_TuitionManagementSystem/JohorSearch.h
+++ Array_TuitionManagementSystem/JohorSearch.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "DataStruc.h"
+using namespace std;
 
 //linear serach tutor array by tutorID
 int JohorLinearSearchTutor(Tutor* tutorArray, int tutorID) {
@@ -18,30 +19,30 @@ void JohorSearchTutor(Tutor tutorArray[])
 {
 	int tutorID;
 
-	std::cout << "Insert Tutor ID :";
-	std::cin >> tutorID;
+	cout << "Insert Tutor ID :";
+	cin >> tutorID;
 
 	int index = JohorLinearSearchTutor(tutorArray, tutorID);
 	if (index == -1)
 	{
 		system("cls");
-		std::cout << endl;
-		std::cout << "Tutor ID not found";
+		cout << endl;
+		cout << "Tutor ID not found";
 	}
 	else
 	{
 		system("cls");
-		std::cout << endl;
-		std::cout << "Tutor ID found" << std::endl;
-		std::cout << "Tutor ID: " << tutorArray[index].tutorID << std::endl;
-		std::cout << "Tutor Name: " << tutorArray[index].tutorName << std::endl;
-		std::cout << "Tutor Date Join: " << tutorArray[index].dateJoin << std::endl;
-		std::cout << "Tutor Date Terminated: " << tutorArray[index].dateTerminated << std::endl;
-		std::cout << "Tutor Hourly Rate: " << tutorArray[index].hourlyRate << std::endl;
-		std::cout << "Tutor Phone: " << tutorArray[index].tutorPhone << std::endl;
-		std::cout << "Tutor Address: " << tutorArray[index].tutorAddress << std::endl;
-		std::cout << "Tutor Tuition Code: " << tutorArray[index].tutionCode << std::endl;
-		std::cout << "Tutor Subject Code: " << tutorArray[index].subjectCode << std::endl;
-		std::cout << "Tutor Rating: " << tutorArray[index].rating;
+		cout << endl;
+		cout << "Tutor ID found" << endl;
+		cout << "Tutor ID: " << tutorArray[index].tutorID << endl;
+		cout << "Tutor Name: " << tutorArray[index].tutorName << endl;
+		cout << "Tutor Date Join: " << tutorArray[index].dateJoin << endl;
+		cout << "Tutor Date Terminated: " << tutorArray[index].dateTerminated << endl;
+		cout << "Tutor Hourly Rate: " << tutorArray[index].hourlyRate << endl;
+		cout << "Tutor Phone: " << tutorArray[index].tutorPhone << endl;
+		cout << "Tutor Address: " << tutorArray[index].tutorAddress << endl;
+		cout << "Tutor Tuition Code: " << tutorArray[index].tutionCode << endl;
+		cout << "Tutor Subject Code: " << tutorArray[index].subjectCode << endl;
+		cout << "Tutor Rating: " << tutorArray[index].rating;
 	}
 };
