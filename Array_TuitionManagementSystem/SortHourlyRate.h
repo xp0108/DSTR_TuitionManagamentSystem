dfcdf5e48d42b636721f08714282de7eaf3b8487
xp <74288201+xp0108@users.noreply.github.com>
2022-05-22 02:30:03 +0800

    update deleted code

--- Array_TuitionManagementSystem/SortHourlyRate.h
+++ Array_TuitionManagementSystem/SortHourlyRate.h
@@ -4,13 +4,6 @@
 #include "DisplayTutor.h"
 using namespace std;
 
-// function to swap the the position of two elements
-void SwapHourlyRate(Tutor* currentCarry, Tutor* minCarry) {
-	Tutor temp = *currentCarry;
-	*currentCarry = *minCarry;
-	*minCarry = temp;
-}
-
 // Loop and Swap - Selection Sort
 void HourlyRateSelectionSort(Tutor tutorArray[]) {
 	int tutorArraySize = 100;
@@ -25,14 +18,24 @@ void HourlyRateSelectionSort(Tutor tutorArray[]) {
 		}
 
 		// put min at the correct position
-		SwapTutorID(&tutorArray[minHourlyRate], &tutorArray[current]);
+		SwapArray(&tutorArray[minHourlyRate], &tutorArray[current]);
 	}
 }
 
 void SortHourlyRate(Tutor tutorArray[]) {
 	system("cls");
 	HourlyRateSelectionSort(tutorArray);
-	cout << "Sorted array in Acsending Order:\n";
-	DisplayTutor(tutorArray);
-	//printArray(tutorArray);
+	DisplayTutor(tutorArray, "Sorted Hourly Pay Rate in Acsending Order");
+}
+
+void SortHourlyRateKL(Tutor tutorArray[]) {
+	system("cls");
+	HourlyRateSelectionSort(tutorArray);
+	DisplayKLTutor(tutorArray, "Sorted Hourly Pay Rate in Acsending Order");
+}
+
+void SortHourlyRateJohor(Tutor tutorArray[]) {
+	system("cls");
+	HourlyRateSelectionSort(tutorArray);
+	DisplayJohorTutor(tutorArray, "Sorted Hourly Pay Rate in Acsending Order");
 }
