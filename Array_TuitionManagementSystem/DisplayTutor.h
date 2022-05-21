61dd97916e4a3fd09e1ad8c7521cd59c8907456f
Kelvin <k3lvintwl2001@gmail.com>
2022-05-21 17:43:29 +0800

    DisplayTutor.h
    Tuition branch convert int to string

--- Array_TuitionManagementSystem/DisplayTutor.h
+++ Array_TuitionManagementSystem/DisplayTutor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include "DataStruc.h"
 
 void DisplayTutor(Tutor tutor_info[]) {
@@ -12,15 +13,23 @@ void DisplayTutor(Tutor tutor_info[]) {
         //Skip NULL values
         if (tutor_info[row].tutorID != NULL) {
 
-            cout << "Tutor ID: " << tutor_info[row].tutorID << endl;
-            cout << "Tutor name: " << tutor_info[row].tutorName << endl;
-            cout << "Date joined: " << tutor_info[row].dateJoin << endl;
-            cout << "Date terminated: N/A" << endl;
-            cout << "Hourly rate: " << tutor_info[row].hourlyRate << endl;
-            cout << "Phone number: " << tutor_info[row].tutorPhone << endl;
-            cout << "Address: " << tutor_info[row].tutorAddress << endl;
-            cout << endl;
+                cout << "Tutor ID: " << tutor_info[row].tutorID << endl;
+                cout << "Tutor name: " << tutor_info[row].tutorName << endl;
+                cout << "Date joined: " << tutor_info[row].dateJoin << endl;
+                cout << "Date terminated: N/A" << endl;
+                cout << "Hourly rate: " << tutor_info[row].hourlyRate << endl;
+                cout << "Phone number: " << tutor_info[row].tutorPhone << endl;
+                cout << "Address: " << tutor_info[row].tutorAddress << endl;
 
+                //Tuition branch print int to string
+                if (tutor_info[row].tutionCode == 1) {
+                    cout << "Tutor Branch: KL Branch"<< endl;
+                }
+                else if (tutor_info[row].tutionCode == 2) {
+                    cout << "Tutor Branch: Johor Branch" << endl;
+                }
+                cout << endl;
+            
         }
     }
 
