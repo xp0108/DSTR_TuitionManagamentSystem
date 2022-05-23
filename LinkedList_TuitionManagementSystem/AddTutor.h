f893d685b0866477571cdacf686d6ce03e5288df
xp <74288201+xp0108@users.noreply.github.com>
2022-05-24 03:49:42 +0800

    update LL function with array
    - modify yw de add code in LL
    - modi my display menu code
    - update struct data (same as array)

--- LinkedList_TuitionManagementSystem/AddTutor.h
+++ LinkedList_TuitionManagementSystem/AddTutor.h
@@ -2,105 +2,153 @@
 #include "DataStruct.h"
 
 bool isEmpty(Tutor* head) {
-    if (head == NULL) {
-        return true;
-    }
-    else {
-        return false;
-    }
+	if (head == NULL) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-void AddNode(Tutor*& head, Tutor*& last, int id, string name, string datejoin,
-    double rate, string phone, string address, int tcode, int scode) {
-    Tutor* newNode = new Tutor;
+//void AddNode(Tutor*& head, Tutor*& last, int id, string name, string datejoin,
+//    double rate, string phone, string address, int tcode, int scode) {
+Tutor* AddNode(int id, string name, string datejoin, double rate, string phone, string address,
+	string tname, string subname, double rating, double paycheck) {
+	Tutor* newNode = new Tutor;
 
-    newNode->tutorID = id;
-    newNode->tutorName = name;
-    newNode->dateJoin = datejoin;
-    newNode->dateTerminated = "N/A";
-    newNode->hourlyRate = rate;
-    newNode->tutorPhone = phone;
-    newNode->tutorAddress = address;
-    newNode->tutionCode = tcode;
-    newNode->subjectCode = scode;
-    head = newNode;
-    last = newNode;
-    cout << endl;
+	// newNode->tutorID = id;
+	// newNode->tutorName = name;
+	// newNode->dateJoin = datejoin;
+	// newNode->dateTerminated = "N/A";
+	// newNode->hourlyRate = rate;
+	// newNode->tutorPhone = phone;
+	// newNode->tutorAddress = address;
+	// newNode->tutionCode = tcode;
+	// newNode->subjectCode = scode;
+	// head = newNode;
+	// last = newNode;
+	// cout << endl;
 
+	newNode->tutorID = id;
+	newNode->tutorName = name;
+	newNode->dateJoin = datejoin;
+	newNode->dateTerminated = "N/A";
+	newNode->hourlyRate = rate;
+	newNode->tutorPhone = phone;
+	newNode->tutorAddress = address;
+	newNode->tutionName = tname;
+	newNode->subjectName = subname;
+	newNode->rating = rating;
+	newNode->payCheck = paycheck;
+	newNode->nextAddress = NULL;
+	return newNode;
 }
 
-void AddTutor(Tutor*& head, Tutor*& last, int id, string name, string datejoin,
-    double rate, string phone, string address, int tcode, int scode) {
-    if (isEmpty(head)) {
-        AddNode(head, last, id, name, datejoin, rate, phone, address, tcode, scode);
-    }
-    else {
-        Tutor* newNode = new Tutor;
+// void AddTutor(Tutor*& head, Tutor*& last, int id, string name, string datejoin,
+//     double rate, string phone, string address, int tcode, int scode) {
+//     if (isEmpty(head)) {
+//         AddNode(head, last, id, name, datejoin, rate, phone, address, tcode, scode);
+//     }
+//     else {
+//         Tutor* newNode = new Tutor;
 
-        newNode->tutorID = id;
-        newNode->tutorName = name;
-        newNode->dateJoin = datejoin;
-        newNode->dateTerminated = "N/A";
-        newNode->hourlyRate = rate;
-        newNode->tutorPhone = phone;
-        newNode->tutorAddress = address;
-        newNode->tutionCode = tcode;
-        newNode->subjectCode = scode;
-        newNode->next = NULL;
-        newNode->next = newNode;
-        last = newNode;
-        cout << endl;
-    }
+//         newNode->tutorID = id;
+//         newNode->tutorName = name;
+//         newNode->dateJoin = datejoin;
+//         newNode->dateTerminated = "N/A";
+//         newNode->hourlyRate = rate;
+//         newNode->tutorPhone = phone;
+//         newNode->tutorAddress = address;
+//         newNode->tutionCode = tcode;
+//         newNode->subjectCode = scode;
+//         newNode->next = NULL;
+//         newNode->next = newNode;
+//         last = newNode;
+//         cout << endl;
+//     }
+// }
+
+void AddTutorToTheEnd(Tutor* newNode) { //i refer to the proposal de algo
+	// check whether the list is empty
+	if (isEmpty(head)) {
+		head = newNode; //if yes, then add newnode as head
+	}
+	else { // list is not empty
+		Tutor* current = head;
+
+		//find the last node in the list
+		while (current->nextAddress != NULL) {
+			current = current->nextAddress;
+		}
+		current->nextAddress = newNode; //assign the newnode to the last
+	}
+
+	::sizeOfLinkedList++; //:: global variable, use to track the linkedlist size (debug)
 }
 
-void Display(Tutor* current) {
-    if (isEmpty(current)) {
-        cout << "The list is empty." << endl;
-    }
-    else {
-        cout << "Content of the list: " << endl;
-        while (current != NULL) {
-            cout << "Tutor ID: " << current->tutorID << endl;
-            cout << "Tutor name: " << current->tutorName << endl;
-            cout << "Date joined: " << current->dateJoin << endl;
-            cout << "Date terminated: " << current->dateTerminated << endl;
-            cout << "Hourly rate: " << current->hourlyRate << endl;
-            cout << "Phone: " << current->tutorPhone << endl;
-            cout << "Address: " << current->tutorAddress << endl;
-            cout << "Tuition code: " << current->tutionCode << endl;
-            cout << "Subject code: " << current->subjectCode << endl;
-            current = current->next;
-            cout << endl;
-        }
-    }
+
+// void Display(Tutor* current) {
+void Display() { // no need pass the pointer, store in memory, jst direct refer
+	Tutor* current = head;
+	if (isEmpty(current)) {
+		cout << "The list is empty." << endl;
+	}
+	else {
+		cout << "Content of the list: " << endl;
+		while (current != NULL) {
+			cout << "Tutor ID: " << current->tutorID << endl;
+			cout << "Tutor name: " << current->tutorName << endl;
+			cout << "Date joined: " << current->dateJoin << endl;
+			cout << "Date terminated: " << current->dateTerminated << endl;
+			cout << "Hourly rate: " << current->hourlyRate << endl;
+			cout << "Phone: " << current->tutorPhone << endl;
+			cout << "Address: " << current->tutorAddress << endl;
+			cout << "Tuition Name: " << current->tutionName << endl;
+			cout << "Subject Name: " << current->subjectName << endl;
+			cout << "Rating: " << current->rating << endl;
+			cout << "Pay Check: " << current->payCheck << endl;
+			current = current->nextAddress;
+			cout << endl;
+		}
+	}
 }
 
 void CallAdd() {
-    Tutor* head = NULL;
-    Tutor* last = NULL;
+	// Tutor* head = NULL;
+	// Tutor* last = NULL;
+	head = NULL; //head var no need define, cuz i make the head as global variable (refer data struct file)
 
-    int id, tcode, scode;
-    double rate;
-    string name, datej, phone, address;
+	int id;
+	double hrate, payCheck;
+	string name, datej, phone, address, tname, sname;
 
-    cout << "Tutor ID: ";
-    cin >> id;
-    cout << "Tutor name: ";
-    cin >> name;
-    cout << "Date joined: ";
-    cin >> datej;
-    cout << "Hourly rate: ";
-    cin >> rate;
-    cout << "Tutor phone: ";
-    cin >> phone;
-    cout << "Tutor address: ";
-    cin >> address;
-    cout << "Tuition code: ";
-    cin >> tcode;
-    cout << "Subject code: ";
-    cin >> scode;
-    cout << endl;
+	cout << "Tutor ID: ";
+	cin >> id;
+	cout << "Tutor name: ";
+	cin >> name;
+	cout << "Date joined: ";
+	cin >> datej;
+	cout << "Hourly rate: ";
+	cin >> hrate;
+	cout << "Tutor phone: ";
+	cin >> phone;
+	cout << "Tutor address: ";
+	cin >> address;
+	cout << "Tuition name: ";
+	cin >> tname;
+	cout << "Subject name: ";
+	cin >> sname;
+	cout << "Pay Check: ";
+	cin >> payCheck;
+	cout << endl;
 
-    AddTutor(head, last, id, name, datej, rate, phone, address, tcode, scode);
-    Display(head);
+	// AddTutor(head, last, id, name, datej, rate, phone, address, tcode, scode);
+	// Display(head);
+
+	Tutor* newNode = AddNode(id, name, datej, hrate, phone, address, tname, sname, 0, payCheck); //create Node
+	AddTutorToTheEnd(newNode);	//add the created node into LL
+	Display();
+
+	//debug
+	cout << "No of tutor in LL: " << ::sizeOfLinkedList << endl;
 }
--- LinkedList_TuitionManagementSystem/Login.h
+++ LinkedList_TuitionManagementSystem/Login.h
@@ -1,162 +1,11 @@
 #pragma once
 #include <iostream>
-#include "DataStruct.h"
-#include "AddTutor.h"
+#include "AdditionalFeature.h"
+#include "UserMenuFunction.h"
+#include "DataStruc.h"
 using namespace std;
 
-void KLAdminMenu()
-{
-	for (;;)
-	{
-		int klAdminChoice;
-		cout << endl << endl;
-		cout << " **KL ADMIN** " << endl;
-		cout << "1. Add rating for tutor" << endl;
-		cout << "2. Search a Tutor by Tutor ID" << endl;
-		cout << "3. Search Tutors by overall performance (rating)" << endl;
-		cout << "4. Sort and display by Tutors ID in ascending order" << endl;
-		cout << "5. Sort and display by Tutors Hourly Pay Rate in ascending order" << endl;
-		cout << "6. Sort and display by Tutors Overall Performance in ascending order" << endl;
-		cout << "7. generate report" << endl;
-		cout << "8. Exit" << endl;
-		cout << "Enter your choice: ";
-		cin >> klAdminChoice;
-		switch (klAdminChoice)
-		{
-		case 1:
-			//function();
-			break;
-		case 2:
-			//function();
-			break;
-		case 3:
-			//function();
-			break;
-		case 4:
-			//function();
-			break;
-		case 5:
-			//function();
-			break;
-		case 6:
-			//function();
-			break;
-		case 7:
-			//function();
-			break;
-		case 8:exit(0);
-			break;
-		default:
-			cout << "Invalid Option! Please Try Again";
-		}
-	}
-}
-
-void JohorAdminMenu()
-{
-	for (;;)
-	{
-		int johorAdminChoice;
-		cout << endl << endl;
-		cout << " **JOHOR ADMIN** " << endl;
-		cout << "1. Add rating for tutor" << endl;
-		cout << "2. Search a Tutor by Tutor ID" << endl;
-		cout << "3. Search Tutors by overall performance (rating)" << endl;
-		cout << "4. Sort and display by Tutors ID in ascending order" << endl;
-		cout << "5. Sort and display by Tutors Hourly Pay Rate in ascending order" << endl;
-		cout << "6. Sort and display by Tutors Overall Performance in ascending order" << endl;
-		cout << "7. generate report" << endl;
-		cout << "8. Exit" << endl;
-		cout << "Enter your choice: ";
-		cin >> johorAdminChoice;
-		switch (johorAdminChoice)
-		{
-		case 1:
-			//function();
-			break;
-		case 2:
-			//function();
-			break;
-		case 3:
-			//function();
-			break;
-		case 4:
-			//function();
-			break;
-		case 5:
-			//function();
-			break;
-		case 6:
-			//function();
-			break;
-		case 7:
-			//function();
-			break;
-		case 8:exit(0);
-			break;
-		default:
-			cout << "Invalid Option! Please Try Again";
-		}
-	}
-}
-
-void HRManagerMenu()
-{
-	for (;;)
-	{
-		int hrChoice;
-		cout << endl << endl;
-		cout << " **HR ADMIN** " << endl;
-		cout << "1. Register Tutor" << endl;
-		cout << "2. Update Tutor Record" << endl;
-		cout << "3. Delete Tutor Record" << endl;
-		cout << "4. Display All Tutor" << endl;
-		cout << "5. Search a Tutor by Tutor ID" << endl;
-		cout << "6. Search Tutors by overall performance (rating)" << endl;
-		cout << "7. Sort and display by Tutors ID in ascending order" << endl;
-		cout << "8. Sort and display by Tutors Hourly Pay Rate in ascending order" << endl;
-		cout << "9. Sort and display by Tutors Overall Performance in ascending order" << endl;
-		cout << "10. Exit" << endl;
-		cout << "Enter your choice: ";
-		cin >> hrChoice;
-		switch (hrChoice)
-		{
-		case 1:
-			CallAdd();
-			break;
-		case 2:
-			//function();
-			break;
-		case 3:
-			//function();
-			break;
-		case 4:
-			//function();
-			break;
-		case 5:
-			//function();
-			break;
-		case 6:
-			//function();
-			break;
-		case 7:
-			//function();
-			break;
-		case 8:
-			//function();
-			break;
-		case 9:
-			//function();
-			break;
-		case 10:exit(0);
-			break;
-		default:
-			cout << "Invalid Option! Please Try Again";
-		}
-	}
-}
-
-void Login()
+void Login(Tutor tutorArray[])
 {
 	int loginCount = 0;
 
@@ -170,18 +19,18 @@ void Login()
 
 		if (username == "kl" && password == "123")
 		{
-			cout << "Here is KL Branch";
-			KLAdminMenu();
+			//cout << "Here is KL Branch";
+			KLAdminMenu(tutorArray);
 		}
 		else if (username == "johor" && password == "123")
 		{
-			cout << "Here is Johor Branch";
-			JohorAdminMenu();
+			//cout << "Here is Johor Branch";
+			JohorAdminMenu(tutorArray);
 		}
 		else if (username == "hr" && password == "123")
 		{
-			cout << "Here is HR Manager";
-			HRManagerMenu();
+			//cout << "Here is HR Manager";
+			HRManagerMenu(tutorArray);
 
 		}
 		else
