47b610d0ff027ea7094826e073061531fc3d79f7
xp <74288201+xp0108@users.noreply.github.com>
2022-05-24 20:21:10 +0800

    Update GenerateData.h

--- LinkedList_TuitionManagementSystem/GenerateData.h
+++ LinkedList_TuitionManagementSystem/GenerateData.h
@@ -4,12 +4,12 @@
 #include "AddTutor.h"
 
 void generateData() {
-	Tutor* newNode1 = AddNode(1, "Onn", "Today", 10, 40, "0127120665", "KL", "KL", "Math", 0, 400); //create Node
+	Tutor* newNode1 = AddNode(1, "Onn", "23/5/2022", 10, 40, "0127120665", "KL", "KL", "Math", 0, 400); //create Node
 	AddTutorToTheEnd(newNode1);
-	Tutor* newNode2 = AddNode(2, "Kelvin", "Today", 10, 60, "0111111111", "Johor", "Johor", "History", 0, 600); //create Node
+	Tutor* newNode2 = AddNode(2, "Kelvin", "1/5/2021", 10, 60, "0111111111", "Johor", "Johor", "History", 0, 600); //create Node
 	AddTutorToTheEnd(newNode2);
-	Tutor* newNode3 = AddNode(3, "YW", "Today", 10, 40, "022222222", "Johor", "Johor", "English", 0, 400); //create Node
+	Tutor* newNode3 = AddNode(3, "YW", "15/5/2020", 10, 40, "022222222", "Johor", "Johor", "English", 0, 400); //create Node
 	AddTutorToTheEnd(newNode3);	
-	Tutor* newNode4 = AddNode(4, "XP", "Today", 10, 80, "022222222", "Johor", "Johor", "History", 0, 800); //create Node
+	Tutor* newNode4 = AddNode(4, "XP", "4/7/2021", 10, 80, "022222222", "Johor", "Johor", "History", 0, 800); //create Node
 	AddTutorToTheEnd(newNode4);	
 }
