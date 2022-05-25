054fd0cd6a1a2b0598d3c1e5746d89914dfc95b1
Kelvin <k3lvintwl2001@gmail.com>
2022-05-25 21:44:29 +0800

    Display forward backward progress
    some progress

--- Array_TuitionManagementSystem/DisplayTutor.h
+++ Array_TuitionManagementSystem/DisplayTutor.h
@@ -6,7 +6,33 @@
 
 void DisplayTutor(Tutor tutor_info[], string displayString = "Tutor Records") {
     system("cls");
-    int arraysize = 100;
+    int currentrow, maxRow, maxPage, arraysize = sizeof(tutor_info), currentPage = 1;
+    int displayMenu;
+
+    //5 rows of tutor's data in each Page
+    if (arraysize % 5 != 0) {
+        maxPage = (arraysize / 5) + 1;
+    }
+    else {
+        maxPage = arraysize / 5;
+    }
+
+    if (currentPage == 0) {
+        currentPage = maxPage;
+    }
+    else if (currentPage > maxPage) {
+        currentPage = 1;
+    }
+
+    if (currentPage == maxPage) {
+        maxRow = 5 * (arraysize / 5) + (arraysize % 5);
+    }
+    else {
+        maxRow = 5 * currentPage;
+    }
+
+    currentrow = (currentPage - 1) * 5;
+
     cout << endl << string(193, '-') << endl << string(85, ' ') << displayString << endl << string(193, '-') << endl;
 
     //Heading
@@ -24,12 +50,12 @@ void DisplayTutor(Tutor tutor_info[], string displayString = "Tutor Records") {
     cout << setw(8) << left << "PayCheck" << "\t | " << endl;
 
     //printing out tutor_info
-    for (int row = 0; row < arraysize; row++) {
+    for (int row = currentrow; row < maxRow; row++) {
 
         //Skip NULL values
         if (tutor_info[row].tutorID != NULL) {
 
-            cout << tutor_info[row].tutorID << "\t| ";
+            cout << "TID" << tutor_info[row].tutorID << "\t| ";
             cout << setw(10) << left << tutor_info[row].tutorName << "\t| ";
             cout << tutor_info[row].dateJoin << "\t| ";
             cout << setw(14) << left << tutor_info[row].dateTerminated << "\t| ";
@@ -45,6 +71,25 @@ void DisplayTutor(Tutor tutor_info[], string displayString = "Tutor Records") {
     }
 
     cout << string(193, '-') << endl;
+    cout << "Page  " << currentPage << " of " << maxPage << endl;
+
+   // cout << "\n1. Next Page" << endl << "2. Back To Menu" << endl << "Enter your choice: ";
+   // while (!(cin >> displayMenu)) {
+   //     cout << endl << "Invalid Input !!!" << endl;
+   //     cout << "Enter you choice again: ";
+   //     cin.clear();
+   //     cin.ignore('\n');
+   // }
+   // switch (displayMenu) 
+   // {
+   // case 1:
+   //     currentPage = currentPage + 1;
+   //     break;
+
+   //     default:
+			//cout << "Invalid Option! Please Try Again";
+   // }
+
 }
 
 
